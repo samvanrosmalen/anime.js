3f2fa4a5ebb1ef0d136d7aac9b39ed77de130321
samvanrosmalen <samvanrosmalen@live.nl>
2021-05-27 11:48:42 +0200

    css toevoegen aan naam

--- main.js
+++ main.js
@@ -4,6 +4,40 @@ anime({
     rotate: 360,
     scale: 3,
     duration: 3000,
-    loop: true
+    loop: true,
+    direction: 'alternate'
 
 });
+
+anime({
+    targets: '.cirkel',
+    keyframes: [
+        {translateY: -40},
+        {translateX: 250},
+        {translateY: 40},
+        {translateX: 0},
+      ],
+      duration: 4000,
+      easing: 'easeOutElastic(1, .8)',
+      loop: true
+});
+
+anime({
+    targets: '#cirkel, #cirkel1, #cirkel2, .el',
+    translateX: function(el) {
+        return el.getAttribute('data-x');
+      },
+      translateY: function(el, i) {
+        return 50 + (-50 * i);
+      },
+      scale: function(el, i, l) {
+        return (l - i) + .25;
+      },
+      rotate: function() { return anime.random(-360, 360); },
+      borderRadius: function() { return ['50%', anime.random(10, 35) + '%']; },
+      duration: function() { return anime.random(1200, 1800); },
+      delay: function() { return anime.random(0, 400); },
+      direction: 'alternate',
+      loop: true
+});
+
